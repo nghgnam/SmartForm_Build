1033aba08264b86e79e1fbf8727cf1fb06603954
nam hoàng <namhate16@gmail.com>
2025-05-21 21:15:34 +0700

    Build base backend

--- src/app/core/models/form-fields/switch-field.model.ts
+++
@@ -1,9 +0,0 @@
-import { FormField } from "./form-fields.model";
-import { FieldType } from "./field-type.enum";
-
-export class SwitchType extends FormField<string>{
-    override get type(): FieldType{
-        return FieldType.SWTICH;
-    }
-
-}
